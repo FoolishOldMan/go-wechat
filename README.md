10973a85b08ea5e31d424e7ae778484f14aad841
zyy6300976 <371558407@qq.com>
2017-06-14 14:13:07 +0800

    AccessToken JSTicket

--- README.md
+++ README.md
@@ -14,6 +14,11 @@ go语言版微信SDK-开发阶段
 
 ## API
 
+- [全局参数](#全局参数)
+    - AccessToken
+		- 获取AccessToken
+    - JSTicket
+		- 获取JSTicket
 - [微信网页开发](#微信网页开发)
     - Oauth2 授权
 		- 获取授权地址
@@ -21,7 +26,24 @@ go语言版微信SDK-开发阶段
 		- 拉取用户信息
 		- 刷新access_token
 		- 检验access_token
-	- 获取js-sdk配置
+
+## 全局参数
+
+### AccessToken
+
+**1.获取AccessToken**
+
+AccessToken的有效期目前为2个小时，需定时刷新，重复获取将导致上次获取的access_token失效。
+
+`GetAccessToken() 获取AccessToken`
+
+### JSTicket
+
+**1.获取JSTicket**
+
+由于获取JSTicket的api调用次数非常有限，频繁刷新JSTicket会导致api调用受限，影响自身业务，开发者必须在自己的服务全局缓存JSTicket。
+
+`GetJSTicket(accessToken string) 获取JSTicket`
 
 ## 微信网页开发
 
--- wechat.go
+++ wechat.go
@@ -1,5 +1,12 @@
 package wechat
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/FoolishOldMan/wechat/util"
+)
+
 var (
 	// AppID appid
 	AppID string
@@ -7,8 +14,63 @@ var (
 	AppSecret string
 )
 
+const (
+	accessTokenURL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
+	ticketURL      = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=%s&type=jsapi"
+)
+
+// AccessToken struct
+type AccessToken struct {
+	util.CommonError
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int64  `json:"expires_in"`
+}
+
+// JSTicket 请求js_ticket返回结果
+type JSTicket struct {
+	util.CommonError
+	Ticket    string `json:"ticket"`
+	ExpiresIn int64  `json:"expires_in"`
+}
+
 // ConfigWechat 微信基本配置
 func ConfigWechat(appID, appSecret string) {
 	AppID = appID
 	AppSecret = appSecret
+}
+
+// GetAccessToken 获取access_token
+func GetAccessToken() (*AccessToken, error) {
+	urlStr := fmt.Sprintf(accessTokenURL, AppID, AppSecret)
+	response, err := http.Get(urlStr)
+	if err != nil {
+		return nil, fmt.Errorf("GetAccessToken error : 请求accessTokenURL异常")
+	}
+	result := AccessToken{}
+	err = util.Response2Struct(response, &result)
+	if err != nil {
+		return nil, fmt.Errorf("GetAccessToken error : 解析accessToken错误 , %v", err.Error())
+	}
+	if result.ErrCode != 0 {
+		return nil, fmt.Errorf("GetAccessToken error : errcode = %v , errmsg = %v", result.ErrCode, result.ErrMsg)
+	}
+	return &result, nil
+}
+
+// GetJSTicket 获取js_ticket
+func GetJSTicket(accessToken string) (*JSTicket, error) {
+	urlStr := fmt.Sprintf(ticketURL, accessToken)
+	response, err := http.Get(urlStr)
+	if err != nil {
+		return nil, fmt.Errorf("GetJSTicket error : 请求ticketURL异常")
+	}
+	result := JSTicket{}
+	err = util.Response2Struct(response, &result)
+	if err != nil {
+		return nil, fmt.Errorf("GetJSTicket error : 解析js_ticket错误 , %v", err.Error())
+	}
+	if result.ErrCode != 0 {
+		return nil, fmt.Errorf("GetJSTicket error : errcode = %v , errmsg = %v", result.ErrCode, result.ErrMsg)
+	}
+	return &result, nil
 }
